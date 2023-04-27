81ec83741f384f9ef36a260e16cb38fd0bd8c7e1
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2023-04-27 18:44:36 -0300

    Add files via upload

---
+++ TP01/ejercicicio_10/Principal.java
@@ -0,0 +1,17 @@
+/*
+Refactoreá la clase CuentaBancaria del ejercicio 3), agregando el atributo titular, que
+representa a la persona titular de la cuenta, y el atributo fechaDeApertura.
+ */
+package ejercicicio_10;
+
+import java.time.LocalDate;
+public class Principal {
+
+    public static void main(String[] args) {
+       
+        //se crea una cuenta bancaria de tipo "Cuenta corriente", con titular "Francisco" y fecha de apertura la fecha actual.
+        CuentaBancaria cb = new CuentaBancaria("Cuenta corriente","Francisco",LocalDate.now());
+        cb.mostrarDatos();
+       
+    }   
+}
