eacd41845283cfec2b93c339c9a3b1537614b766
Francisco Chiminelli <franciscofchiminelli@hotmail.com>
2023-05-26 20:42:15 -0300

    Add files via upload

---
+++ TP03/ejercicio_07/src/ejercicio_07/GrupoDeFiguras.java
@@ -0,0 +1,29 @@
+package ejercicio_07;
+
+import java.util.ArrayList;
+
+/**
+* Código generado por la app UXFtoJava by Charly Cimino
+* @see https://github.com/CharlyCimino/uxf-to-java
+*/
+public class GrupoDeFiguras {
+
+    private ArrayList<Figura> figuras;
+
+    public GrupoDeFiguras() {
+        figuras = new ArrayList<Figura>();
+    }
+    
+    public void agregarFigura(Figura figura1) {
+        figuras.add(figura1);
+    }
+
+    public void mostrarInfoDeFiguras() {
+        for(Figura f: figuras){
+            System.out.println("Color: " + f.getColor());
+            System.out.println("Área: " + f.area());
+            System.out.println("Perímetro: " + f.perimetro());
+            System.out.println();
+        }
+    }
+}
